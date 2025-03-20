c6c7c7465239cb1bbf18d62d179ff57d8fdfc823
Kheang <thormengkheang@gmail.com>
2025-03-20 15:53:23 +0700

    feat: update agenda section (#18)
    * remove contact section
    
    * update agenda

---
+++ src/data/agenda.ts
@@ -0,0 +1,166 @@
+export const agendasMorning = [
+  {
+    id: 1,
+    time: "8:00 - 8:30",
+    title: "Arrival and Registration",
+  },
+  {
+    id: 2,
+    time: "8:30 - 8:35",
+    title: "National Anthem and Welcome",
+  },
+  {
+    id: 3,
+    time: "8:35 - 8:45",
+    title: "Opening Remarks",
+    remarks: [
+      "H.E. Prof. Hor Peng, Rector of National University of Management",
+    ],
+  },
+  {
+    id: 4,
+    time: "8:45 - 8:55",
+    title: "Remarks from Organizers",
+    remarks: ["NUM Faculty of Digital Economy and Startup Grind"],
+  },
+  {
+    id: 5,
+    time: "8:55 - 9:00",
+    title: "Showcase Video",
+  },
+  {
+    id: 6,
+    time: "9:00 - 9:05",
+    title: "Photo Session",
+  },
+  {
+    id: 7,
+    time: "9:05 - 9:55",
+    title: "Panel Discussion 1: Roles of Blockchain in Cambodia’s Economy",
+    remarks: [
+      "Dr. Daiju Aiba (National Bank of Cambodia)",
+      "Dr. Sei Socheat (NUM Faculty of Digital Economy)",
+      "Serey Vathana Tum (Techo Startup Center)",
+      "Representative from Ministry of Industry, Science, Technology & Innovation (MISTI)",
+      "Representative from Ministry of Post and Telecommunications (MPTC)",
+      "Moderator: Freya Lou",
+    ],
+  },
+  {
+    id: 8,
+    time: "9:55 - 10:15",
+    title: "Q&A Session with Audience",
+  },
+  {
+    id: 9,
+    time: "10:15 - 10:40",
+    title: "Break",
+  },
+  {
+    id: 10,
+    time: "10:40 - 11:50",
+    title: "Blockchain Startup Project Showcase",
+    remarks: [
+      "Actik",
+      "AURA",
+      "COTA",
+      "CRAFT-I",
+      "DiTY",
+      "Melozic",
+      "NID",
+      "Passify",
+      "Plusskill",
+      "Srork Yeung",
+      "T-Rental",
+      "TCI3",
+    ],
+  },
+  {
+    id: 11,
+    time: "11:50 - 12:00",
+    title: "Certificate of Appreciation & Photo Session",
+  },
+  {
+    id: 12,
+    time: "12:00 - 13:00",
+    title: "Lunch Break",
+  },
+];
+
+export const agendasAfternoon = [
+  {
+    id: 1,
+    time: "13:00 - 13:30",
+    title: "Arrival and Registration",
+  },
+  {
+    id: 2,
+    time: "13:30 - 13:45",
+    title: "Keynote Presentation",
+    remarks: ["Thomas Schings (RGX)"],
+  },
+  {
+    id: 3,
+    time: "13:45 - 14:15",
+    title: "Fireside Chat",
+    remarks: ["Luis Filipe Goncalves Arieira"],
+  },
+  {
+    id: 4,
+    time: "14:15 - 15:15",
+    title: "Workshop Breakout Sessions",
+    remarks: [
+      "Saing Sab (IG Tech Group)",
+      "Khuon Sophorth (Morokot)",
+      "Dr. Steve Peters (wallet79)",
+      "Sambat Lim",
+      "Dreamslab",
+    ],
+  },
+  {
+    id: 5,
+    time: "15:15 - 15:35",
+    title: "Break",
+  },
+  {
+    id: 6,
+    time: "15:35 - 15:45",
+    title: "Interactive Activities / Games",
+  },
+  {
+    id: 7,
+    time: "15:45 - 16:25",
+    title: "Panel Discussion 2: Real Use Cases of Blockchain Technology",
+    remarks: [
+      "Chhay Lem Lim (Serey Platform)",
+      "Ley (Personstage)",
+      "Kris (Personstage)",
+      "Jason (Xertificate)",
+      "Moderator: Oumkanal Chun",
+    ],
+  },
+  {
+    id: 8,
+    time: "16:25 - 16:45",
+    title: "Q&A Session with the Audience",
+  },
+  {
+    id: 9,
+    time: "16:45 - 16:50",
+    title: "Closing Remark",
+    remarks: ["Freya Lou"],
+  },
+  {
+    id: 10,
+    time: "16:50 - 17:00",
+    title: "Certificate of Appreciation & Photo Session",
+  },
+];
+
+export const agendasEvening = [
+  {
+    id: 1,
+    time: "17:30 - 21:00",
+    title: "Community Meetup (VIP and Invitation Only)",
+  },
+];
