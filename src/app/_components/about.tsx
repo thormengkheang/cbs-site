b958600a14ecc5522b2d670bbeca9c7e7d022887
Kheang <thormengkheang@gmail.com>
2025-03-22 15:43:05 +0700

    feat: update countdown (#26)
    * update about
    
    * update location
    
    * update date & link

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -18,14 +18,12 @@ export const About = ({ ref }: { ref: Ref<HTMLElement> }) => {
             <div className="flex flex-col gap-4 pt-4 sm:flex-row">
               <div className="flex items-center gap-2">
                 <Calendar className="h-5 w-5 text-purple-400" />
-                <span className="text-gray-300">
-                  Sunday, March 30, 8:00 AM - 5:00 PM
-                </span>
+                <span className="text-gray-300">March 30, 2025</span>
               </div>
               <div className="flex items-center gap-2">
                 <MapPin className="h-5 w-5 text-purple-400" />
                 <span className="text-gray-300">
-                  National University of Management Phnom Penh, Cambodia
+                  National University of Management
                 </span>
               </div>
             </div>
